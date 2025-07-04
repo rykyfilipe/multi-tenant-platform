32dc15a1d974acb995acba4276438e781336222c
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-07-04 16:42:16 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 /** @format */
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
+	},
+	webpack: (config, { isServer }) => {
+		// Avoid scanning unnecessary system directories
+		config.snapshot = {
+			...(config.snapshot || {}),
+			managedPaths: [],
+			immutablePaths: [],
+		};
+		return config;
 	},
 };
 
