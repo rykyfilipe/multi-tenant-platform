c085f4d100ab3c256791ee002d48d8df2a7e41dd
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-08-03 17:45:34 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,14 @@
 /** @format */
 
+import GoogleProvider from "next-auth/providers/google";
+import { Session } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
+import type { Adapter } from "next-auth/adapters";
+import { JWT } from "next-auth/jwt";
+import prisma from "@/lib/prisma";
+import { verifyPassword } from "@/lib/auth";
+import { generateToken, JWT_SECRET,authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
