74cc72233a3a75d939bf1aee9d7420ea402df8f7
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-08-05 16:42:34 +0300

    fix

--- src/hooks/useColumnsTableEditor.ts
+++ src/hooks/useColumnsTableEditor.ts
@@ -56,8 +56,7 @@ function useColumnsTableEditor() {
 				updateData[fieldName] = value ? Number(value) : null;
 			} else if (
 				fieldName === "required" ||
-				fieldName === "primary" ||
-				fieldName === "autoIncrement"
+				fieldName === "primary" 
 			) {
 				updateData[fieldName] = Boolean(value);
 			} else {
